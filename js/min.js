9028edca2d8a820277fbc69c5d701fcfd3b50195
Mustafamahammad <mostafamuhammad19@gmail.com>
2020-06-27 17:21:59 +0200

    initial commit

---
+++ js/min.js
@@ -0,0 +1,27 @@
+/*
+$(document).ready(function(){
+$('.slickslider').slick({
+});
+});
+
+$('.mainslider').slick({
+    
+        
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    autoplay:true,
+    rows:3,
+        
+    });
+    */
+
+var typed = $(".typed");
+
+$(function() {
+    typed.typed({
+    strings: ["Best Deals on Mobile Phones"],
+    typeSpeed: 0.5,
+    loop: false,
+    });
+});
+
